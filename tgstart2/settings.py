e6e4022807b7f4987b01a510d03adef631de1307
zaktsy <ibudenov2@gmal.com>
2021-06-04 15:13:38 +0500

    A file was deleted

--- tgstart2/settings.py
+++ tgstart2/settings.py
@@ -40,7 +40,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     # 'lk.apps.LkConfig',
-    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
